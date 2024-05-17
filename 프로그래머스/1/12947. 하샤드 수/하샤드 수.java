949219e524c5ca9e194edd641d5ea6ec9cf5f96d
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-05-17 18:38:44 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum=0;
+        String s=Integer.toString(x);
+        
+        for(int i=0;i<s.length();i++){
+            sum+=s.charAt(i)-'0';
+        }
+       
+        if(x%sum==0) return true;
+        else return false;
+    }
+}
