e86201af3057def460c628a630108472e11dcacd
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-05-18 09:56:42 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum=0;
+        
+        for(int i=0;i<signs.length;i++){
+            if(signs[i]) sum+=absolutes[i];
+            else sum+=absolutes[i]*-1;
+            
+        }
+        
+       
+        return sum;
+    }
+}
