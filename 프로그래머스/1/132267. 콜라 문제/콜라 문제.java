f64eb2758ca2d71862da95ef8015ef6204c015a7
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-05-19 21:55:06 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int get=0;
+        int answer=0;
+        while(n>=a){
+            get=(n/a)*b;
+            n=n%a+get;
+            answer+=get;
+        }
+        return answer;
+    }
+}
