b134c8b357208e2e29fe5dd823f044a2b0323db5
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-05-20 16:16:22 +0900

    [level 1] Title: 모의고사, Time: 3.88 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,29 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] students={
-            {1, 2, 3, 4, 5},
-            {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-        int[] cnt=new int[3];
+        
+        int[] st1={1, 2, 3, 4, 5};
+        int[] st2={2, 1, 2, 3, 2, 4, 2, 5};
+        int[] st3={3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score={0,0,0};
         
         for(int i=0;i<answers.length;i++){
-            for(int j=0;j<3;j++){
-                if(answers[i]==students[j][i%students[j].length]) cnt[j]++;
-            }
+            if(st1[i%5]==answers[i]) score[0]++;
+            if(st2[i%8]==answers[i]) score[1]++;
+            if(st3[i%10]==answers[i]) score[2]++;
+        }
+        int max=score[0];
+        for(int i=1;i<3;i++){
+            if(score[i]>max) max=score[i];
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<3;i++){
+            if(max==score[i]) list.add(i+1);
         }
         
-        int max=0;
-        for(int c:cnt){
-            if(c>max) max=c;
-        }
-        List<Integer> result=new ArrayList<>();
-        for(int i=0;i<3;i++) {
-            if(cnt[i]==max) result.add(i+1);
-            }
+        int[] answer=list.stream().mapToInt(Integer::intValue).toArray();
+            
         
-        return result.stream().mapToInt(i->i).toArray();
+        
+        return answer;
     }
 }
