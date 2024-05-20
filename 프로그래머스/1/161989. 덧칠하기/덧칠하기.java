85edf7d5f9e9731a308d73a3d2847b596a2c79c1
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-05-20 18:35:23 +0900

    [level 1] Title: 덧칠하기, Time: 0.76 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,26 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        StringBuilder sb=new StringBuilder("o".repeat(n));
-        for(int w:section) sb.setCharAt(w-1,'x');
-        String wall=sb.toString();
+        int roller=0;
         
-        while(wall.indexOf('x')!=-1){
-             int index=wall.indexOf('x');
-             if(index>=n-m){
-                 answer++;
-                 break;
-             }
-             wall=wall.substring(0,index)+"o".repeat(m)+wall.substring(index+m);
-             answer++;
-         }
-        
-        // for(int w:section){
-        //     if(wall.charAt(w-1)=='x'){
-        //         if(w<n-m+1) wall.replace(w-1,w+m-1,"o".repeat(m));
-        //         answer++;
-        //     }
-        // }
+        for(int s:section){
+            if(s>roller){
+                roller=s+m-1;
+                answer++;
+            }
+        }
         
         return answer;
     }
