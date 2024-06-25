8404b10e3999625022d60b28ad655059a4d83540
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-06-25 14:23:45 +0900

    [level 2] Title: 기능개발, Time: 2.53 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,24 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] days=new int[progresses.length];
+        // int[] days=new int[progresses.length];
+        Queue<Integer> days=new LinkedList<>();
         for(int i=0;i<progresses.length;i++){
             int remain=100-progresses[i];
-            if(remain%speeds[i]==0) days[i]=remain/speeds[i];
-            else days[i]=(remain/speeds[i])+1;
+            if(remain%speeds[i]==0) days.offer(remain/speeds[i]);
+            else days.offer((remain/speeds[i])+1);
         }
         
         List<Integer> list=new ArrayList<>();
         
+        
         int cnt=1;
-        int max=days[0];
-        for(int i=1;i<days.length;i++){
-            if(days[i]>max) {
+        int max=days.poll();
+       while(!days.isEmpty()){
+            if(days.peek()>max) {
                 list.add(cnt);
                 cnt=1;
-                max=days[i];
+                max=days.poll();
             }else{
                 cnt++;
+                days.poll();
             }
             
         }
