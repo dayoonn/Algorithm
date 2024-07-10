957459176a17cf3663f6030242293668946a003f
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-07-10 18:53:36 +0900

    [level 2] Title: 땅따먹기, Time: 28.09 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int[][] dp=new int[land.length][4];
+        for(int i=0;i<4;i++) dp[land.length-1][i]=land[land.length-1][i];
+        
+        for(int i=land.length-2;i>=0;i--){
+            dp[i][0]=land[i][0]+Math.max(Math.max(dp[i+1][1],dp[i+1][2]),dp[i+1][3]);
+            dp[i][1]=land[i][1]+Math.max(Math.max(dp[i+1][0],dp[i+1][2]),dp[i+1][3]);
+            dp[i][2]=land[i][2]+Math.max(Math.max(dp[i+1][1],dp[i+1][0]),dp[i+1][3]);
+            dp[i][3]=land[i][3]+Math.max(Math.max(dp[i+1][1],dp[i+1][2]),dp[i+1][0]);
+           
+        }
+        
+        int answer=0;
+        for(int i=0;i<4;i++){
+            answer=Math.max(dp[0][i],answer);
+        }
+
+        return answer;
+    }
+}
