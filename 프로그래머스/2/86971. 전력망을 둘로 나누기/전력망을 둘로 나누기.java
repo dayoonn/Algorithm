777c57241f3955fb97820dc120d298ca2f27b12e
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-11-26 21:40:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.58 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,57 +1,62 @@
 import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] wires) {
         int answer = 100;
         
-        // 인접 리스트 생성
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            list.add(new ArrayList<>());
-        }
+        for(int i=0;i<n;i++) list.add(new ArrayList<>());
         
-        // 초기 세팅
-        for (int[] w : wires) {
-            int n1 = w[0] - 1;
-            int n2 = w[1] - 1;
+        //초기 세팅
+        for(int[] w : wires){
+            int n1 = w[0]-1;
+            int n2 = w[1]-1;
             list.get(n1).add(n2);
             list.get(n2).add(n1);
         }
         
-        // 탐색
-        for (int i = 0; i < n - 1; i++) {
-            int n1 = wires[i][0] - 1;
-            int n2 = wires[i][1] - 1;
+//         for(List<Integer> l : list){
+//             for(Integer ll : l) System.out.print(ll+"/");
+//             System.out.println();
+//         }
+        
+        //탐색
+        for(int i=0;i<n-1;i++){
             
-            // 연결을 끊고 탐색
+            int n1 = wires[i][0]-1;
+            int n2 = wires[i][1]-1;
+            int cnt=0;
+            // System.out.println();
+            // System.out.println("**********"+(n1+1)+" "+(n2+1));
+            
             boolean[] visited = new boolean[n];
-            int cnt = bfs(list, visited, n1, n2); // n1에서 출발, n2는 연결 끊음
+            Queue<Integer> que = new LinkedList<>();
             
-            // 두 네트워크의 차이 계산
-            int diff = Math.abs((n - cnt) - cnt);
-            answer = Math.min(answer, diff);
+            que.add(n1);
+            visited[n1]= true;
+            cnt++;
+            while(!que.isEmpty()){
+                int out = que.poll(); //꺼낸 숫자 0~
+                if (!visited[out]) {
+                    cnt++;
+                    // System.out.print((out+1)+"/");
+                } //방문 안했으면 카운트
+                visited[out] = true; //방문 체크
+                
+                for(int k=0;k<list.get(out).size();k++){
+                    int num = list.get(out).get(k);
+                    if(num==n2) continue;
+                    if(!visited[num]) que.add(num);
+                }
+                
+            }
+            int diff = (n-cnt) - cnt;
+            if(diff == 0) return 0;
+            diff = diff<0 ? -diff:diff;
+            
+            if(answer>diff) answer=diff;
+            
         }
         
         return answer;
-    }
-    
-    private int bfs(List<List<Integer>> list, boolean[] visited, int start, int skip) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
-        int count = 1;
-        
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            
-            for (int neighbor : list.get(node)) {
-                if (neighbor == skip || visited[neighbor]) continue; // 연결 끊긴 노드 무시
-                visited[neighbor] = true;
-                queue.add(neighbor);
-                count++;
-            }
-        }
-        
-        return count;
     }
 }
